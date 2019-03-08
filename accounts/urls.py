8efe6c04221cdef4006c4193e36b636ca12db67a
lazy_geek <vincentedepaulo@gmail.com>
2019-03-08 13:49:30 +0300

    A login and Logout Authentication system

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.accounts)
+    path('', views.accounts, name="home"),
+    path('login/', accounts.user_login, name="login"),
+    path('logout/', accounts.user_logout, name="logout")
+    path('book/', views.book, name="book")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,47 @@
+from django.contrib.auth import authenticate, logout, login
+from django.urls import reverse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import User, Doctor, Appointment
 
 # Create your views here.
 
 def accounts(request):
+    if not request.user.authenticated:
+        return render(request, 'accounts/login.html', {"message": None})
     context = {
-        "title": "Home",
+        "user": user.request
     }
-    return render(request, 'accounts/base.html', context=context)
+    return render(request, 'accounts/index.html', context=context)
+def login(request):
+    email = request.POST["email"]
+    password = request.POST["password"]
+
+    user = authenticate(request, email=email, password = password)
+
+    if user is None:
+        return render(request, "accounts/login.html", {"message": "Invalid Credentials"})
+    else:
+        login(request, user)
+        return HttpResponseRedirect(reverse('home'))
+
+def logout(request):
+    logout(request)
+    return render(request, "accounts/login.html", {"message": "Logged out Successfully!"})
+    
+
+
+def book(request, user_id ):
+    try:
+        doctor_id = int (request.POST['doctor'])
+        user = User.objects.get(pk=user_id)
+        doctor = Doctor.objects.get(pk=doctor_id)
+    except KeyError:
+        return render(request, 'accounts/error.html', {"message": "Make Sure you Select Doctor and User"})
+    except User.DoesNotExist:
+        return render(request, 'accounts/error.html', {"message": "No user."})
+    except Doctor.DoesNotExist:
+        return render(request, 'accounts/error.html', {"message": "No Doctor."})
+    
+
+
