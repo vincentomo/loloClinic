5acc164f1a4e844e4f65792c51b2e443da15871f
lazy_geek <vincentedepaulo@gmail.com>
2019-03-08 10:40:27 +0300

    Creating a Models for Doctors, Users, Appointment plus a new django-admin setup plus setup for mysql database, and basic settings urls

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    firstName = models.CharField(max_length=60)
+    lastName = models.CharField(max_length=60)
+    surname = models.CharField(max_length=60)
+    emailAddress = models.EmailField(max_length=60)
+    password = models.CharField(max_length=100)
+    profession = models.CharField(max_length=60)
+    country = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.firstName} {self.lastName} - {self.profession}"
+
+
+class Doctor(models.Model):
+    firstName = models.CharField(max_length=60)
+    lastName = models.CharField(max_length=60)
+    surname = models.CharField(max_length=60)
+    emailAddress = models.EmailField(max_length=60)
+    password = models.CharField(max_length=100)
+    workgroup = models.CharField(max_length=10)
+    speciality = models.CharField(max_length=60)
+    country = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.firstName} {self.lastName} - {self.speciality}"
+
+
+class Appointment(models.Model):
+    title = models.CharField(max_length=80, null=False, blank=False)
+    ref_code = models.CharField(max_length=20)
+    patient = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+    doctor = models.OneToOneField(Doctor, on_delete=models.SET_NULL, null=True)
+    description = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    active = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return f"{self.title}" 
+
+
+
+
+    
--- loloAfya/settings.py
+++ loloAfya/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'suit',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,14 @@ WSGI_APPLICATION = 'loloAfya.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'loloafya',
+        'USER': 'root',
+        'PASSWORD': 'michael12',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
